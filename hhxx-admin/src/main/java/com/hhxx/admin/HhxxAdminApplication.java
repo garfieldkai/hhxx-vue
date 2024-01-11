4d7fbf3594b7a6577be5a0390ff34ac53ddc86ca
田凯 <garfieldkai@126.com>
2024-01-11 11:38:01 +0800

    Initial commit

---
+++ hhxx-admin/src/main/java/com/hhxx/admin/HhxxAdminApplication.java
@@ -0,0 +1,19 @@
+package com.hhxx.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ *
+ * @author TK
+ */
+
+@SpringBootApplication
+public class HhxxAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhxxAdminApplication.class, args);
+    }
+
+}
